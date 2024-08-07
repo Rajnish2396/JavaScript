a1cbb90f9ed01fc40b18bffe27badba99a83017c
Rajnish2396 <rajnishuiux@gmail.com>
2024-08-07 09:02:59 +0530

    learn debugger

---
+++ debugger.js
@@ -0,0 +1,19 @@
+/*
+    Q. What is a debugger ?
+    A. With the help of debugger we can see how to work javascript
+    Q. How to show debuger view
+    A. right clik>inspect> then source tab from brouser (debuger tool) + press "fn+f9" key of keyword
+*/
+debugger
+let fname = "Rajnish";
+let lname = "Bharti";
+let age = 20;
+let userDtls = `Hello, My name is ${fname} ${lname}. and My age is ${age}`
+console.log(userDtls)
+
+/*
+  Code Summery :
+   ==>first line memory set
+   ==> second line value assing in variable
+  
+*/
